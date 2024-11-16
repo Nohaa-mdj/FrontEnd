b743e97079e0c93c10599153d3d297af6748f9c2
Noha <nohaa.mdj@gmail.com>
2024-11-16 04:07:42 +0100

    authentification login

--- login.js
+++ login.js
@@ -1,23 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {      //car ne détecte pas le loginform du html
+
 const loginApi = "http://localhost:5678/api/users/login"
 
 
-async function handleSubmit() {
+document. getElementById('loginform').addEventListener ("submit", () => handleSubmit)
+
+async function handleSubmit(event) {
+
+    event.preventDefault()
 
     let user = {
-        email: 'sophie.bluel@test.tld',
-        password: 'S0phie'
-     } 
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value
+     }
+
         
      let response = await fetch(loginApi, {
         method: "POST",
         headers: {
         "Content-Type" : "application/json"
         },
-        body: JSON.stringify(user)
+        body: JSON.stringify(user),
      }) 
     
      let result = await response.json()
      console.log(result)
+     console.log("E-mail:", email)
+     console.log("Mot de passe:", password)
 }
-
-handleSubmit()
+})
