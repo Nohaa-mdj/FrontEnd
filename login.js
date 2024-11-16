5d76b13bfbe6becabfed2a3c766e27f281d2fbb8
Noha <nohaa.mdj@gmail.com>
2024-11-16 03:06:25 +0100

    création fichier login js

---
+++ login.js
@@ -0,0 +1,8 @@
+const login = "http://localhost:5678/api/users/login"
+
+const submitButton = document.querySelector("submitButton")
+submitButton.addEventListener("click", () => handleSubmit())
+
+function handleSubmit(email, password) {
+
+}
