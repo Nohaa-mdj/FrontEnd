36cbdb0a9925c5577322718dedc4ddaefa2a47e7
Noha <nohaa.mdj@gmail.com>
2024-11-16 11:16:46 +0100

    modif authentification

--- login.js
+++ login.js
@@ -1,31 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {      //car ne détecte pas le loginform du html
 
-const loginApi = "http://localhost:5678/api/users/login"
+    const loginApi = "http://localhost:5678/api/users/login"
 
 
-document. getElementById("loginform").addEventListener ("submit", () => handleSubmit)
+    document. getElementById("loginform").addEventListener ("submit", handleSubmit)
 
-async function handleSubmit(event) {
+    async function handleSubmit(event) {
 
-    event.preventDefault()
+        event.preventDefault()
 
-    let user = {
-        email: document.getElementById("email").value,
-        password: document.getElementById("password").value
-     }
+        let user = {
+            email: document.getElementById("email").value,
+            password: document.getElementById("password").value
+        }
 
+            
+        let response = await fetch(loginApi, {
+            method: "POST",
+            headers: {
+            "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(user),
+        }) 
         
-     let response = await fetch(loginApi, {
-        method: "POST",
-        headers: {
-        "Content-Type" : "application/json"
-        },
-        body: JSON.stringify(user),
-     }) 
-    
-     let result = await response.json()
-     console.log(result)
-     console.log("E-mail:", email)
-     console.log("Mot de passe:", password)
-}
+        let result = await response.json()
+        console.log(result)
+        console.log("E-mail:", email)
+        console.log("Mot de passe:", password)
+    }
 })
