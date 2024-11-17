29c1ff74e501b9546163afa9331a955dd3c45c98
Noha <nohaa.mdj@gmail.com>
2024-11-17 20:26:15 +0100

    création gallery modal + logout

--- projet.js
+++ projet.js
@@ -15,10 +15,12 @@ async function afficherGalerie(filter) {
             const filtered = json.filter((data) => data.categoryId === filter);
             for (let i = 0; i < filtered.length; i++) {
                 setFigure(filtered[i]);
+                setModalFigure(filtered[i])
             }
         } else {
             for (let i = 0; i < json.length; i++) {
                 setFigure(json[i]);
+                setModalFigure(json[i])
             }
         }
     } catch (error) {
@@ -33,6 +35,13 @@ function setFigure(data) {
     figure.innerHTML = `<img src="${data.imageUrl}" alt="${data.title}">
                         <figcaption>${data.title}</figcaption>`
     document.querySelector(".gallery").append(figure)
+}
+
+function setModalFigure(data) {
+    const figure = document.createElement("figure");
+    figure.innerHTML = `<img src="${data.imageUrl}" alt="${data.title}">
+                        <figcaption>${data.title}</figcaption>`
+    document.querySelector(".gallery-modal").append(figure)
 }
 
 document.addEventListener("DOMContentLoaded", () => {           //être sûr que toutes les images s'affichent au début
@@ -80,6 +89,8 @@ function displayAdminMode() {
         editBanner.className = "edit"
         editBanner.innerHTML = '<p><a href="#modal1" class="js-modal"><i class="fa-regular fa-pen-to-square"></i>Mode édition</a></p>'
         document.body.prepend(editBanner)
+
+        document.querySelector(".login").textContent = "logout"
     }
 }
 
